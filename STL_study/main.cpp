65bcb2f50b82a6270d7407bf51f6a1a1c7e55c4b
dingpeng <victorp_460@163.com>
2021-03-04 15:08:54 +0800

    Initial Commit

---
+++ STL_study/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  STL_study
+//
+//  Created by Ding Peng on 2021/3/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
